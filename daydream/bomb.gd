d82ec1e4776959ffc745ea4f7a1c3d95ce295e2f
idksarah <sarah.p.ngai@gmail.com>
2025-09-27 16:29:57 -0700

    bomb

---
+++ daydream/bomb.gd
@@ -0,0 +1,25 @@
+extends RigidBody3D
+
+@onready var debris = $Debris
+var body
+@onready var timer = $Timer
+var explosion_timeout = 2
+var animation_timeout = .2
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	timer.wait_time = explosion_timeout
+	timer.start()
+	print("bomb init")
+		
+func _process(delta: float) -> void:
+	if timer.time_left == 0:
+		debris.emitting = true
+		await get_tree().create_timer(animation_timeout).timeout
+		queue_free()
+	
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	print("bomb entered")
+	if (!(body is Fence)): return
+	await get_tree().create_timer(timer.time_left + animation_timeout).timeout
+	body.queue_free()
