b0702d71e8c0d21e1d542de57a541301c88d2a4b
klock1125 <klock1125@gmail.com>
2025-09-28 14:08:33 -0700

    dasjdls

--- daydream/car.gd
+++ daydream/car.gd
@@ -1,14 +1,29 @@
 class_name Car
 extends RigidBody3D
 
+@onready var CarModel = $CarModel
+
+var red_car_path = "res://Assets/Red-car/RedCar.obj"
+var blue_car_path = "res://Assets/Blue-car/BlueCar.obj"
+var yellow_car_path = "res://Assets/Yellow-car/YellowCar.obj"
+
 var goingLeft = true
 var speed = .075
+
+var rand = randi() % 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	goingLeft = bool(randi() % 2)
 	if !goingLeft:
 		rotate_y(Singleton.pi)
+	if rand == 0:
+		CarModel.mesh = load(red_car_path)
+	if rand == 1:
+		CarModel.mesh = load(blue_car_path)
+	if rand == 2:
+		CarModel.mesh = load(yellow_car_path)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
